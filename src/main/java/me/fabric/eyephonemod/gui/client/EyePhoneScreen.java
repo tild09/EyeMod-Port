70680e70e24ea073d5e14a939442e75244aad5aa
Ye Yu <rafolwen98@gmail.com>
2020-11-11 00:19:07 +0800

    Make dummy gui for eyephone - 2

--- src/main/java/me/fabric/eyephonemod/gui/client/EyePhoneScreen.java
+++ src/main/java/me/fabric/eyephonemod/gui/client/EyePhoneScreen.java
@@ -1,98 +1,32 @@
 package me.fabric.eyephonemod.gui.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import me.fabric.eyephonemod.gui.client.element.CenteredPanel;
+import me.fabric.eyephonemod.EyePhoneMod;
 import me.fabric.eyephonemod.gui.handler.ClientScreenHandler;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.render.DiffuseLighting;
-import net.minecraft.client.util.math.MatrixStack;
+import me.fabric.eyephonemod.gui.handler.eyephone.EyePhoneClientScreenHandler;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
-import org.lwjgl.opengl.GL11;
 
-import java.util.ArrayList;
+public class EyePhoneScreen<T extends ClientScreenHandler> extends BaseScreen<T> {
+    public static final TextureSetting BG_TEXTURE = new TextureSetting(
+            new Identifier(EyePhoneMod.NAMESPACE, "textures/background/default.png"),
+            512, 512
+    );
 
-public abstract class EyePhoneScreen<T extends ClientScreenHandler> extends HandledScreen<T> {
-
-    ArrayList<CenteredPanel> parents = new ArrayList<>();
+    final EyePhoneClientScreenHandler handler;
 
     public EyePhoneScreen(@NotNull T handler, @NotNull PlayerInventory inventory, @NotNull Text title) {
         super(handler, inventory, title);
-        this.backgroundWidth = 248;
-        this.backgroundHeight = 166;
+        if (!(handler instanceof EyePhoneClientScreenHandler))
+            throw new RuntimeException("Handler must be a DummyClientScreenHandler type!");
+        this.handler = (EyePhoneClientScreenHandler) handler;
+        this.backgroundWidth = 201;
+        this.backgroundHeight = 256;
     }
 
     @Override
-    public void init(MinecraftClient client, int width, int height) {
-        super.init(client, width, height);
-        parents.forEach(e -> e.init(width, height));
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-    }
-
-    public ClientScreenHandler getEyePhoneScreenHandler() {
-        return handler;
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.color4f(1.0f, 1.0f, 1.0f, 1.0f);
-        final TextureSetting texture = getTexture();
-        MinecraftClient.getInstance().getTextureManager().bindTexture(texture.textureId);
-        drawTexture(
-                matrices,
-                x,
-                y,
-                getZOffset(),
-                (float) texture.offsetX,
-                (float) texture.offsetY,
-                backgroundWidth,
-                backgroundHeight,
-                texture.height,
-                texture.width
-                );
-    }
-
-    abstract TextureSetting getTexture();
-
-    @SuppressWarnings("deprecation")
-    @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        drawBackground(matrices, delta, mouseX, mouseY);
-        parents.forEach(p -> p.queryMouseOver(mouseX, mouseY));
-        final MinecraftClient mc = MinecraftClient.getInstance();
-        GL11.glEnable(GL11.GL_SCISSOR_TEST);
-        GL11.glScissor(0, 0, mc.getWindow().getFramebufferWidth(), mc.getWindow().getFramebufferHeight());
-        parents.forEach (p -> p.render(matrices, mouseX, mouseY, delta));
-        GL11.glDisable(GL11.GL_SCISSOR_TEST);
-        DiffuseLighting.disable();
-        // Needed because super.render leaves dirty state
-        // draw cursor item stack
-
-        final ItemStack cursorStack = playerInventory.getCursorStack();
-        if (!cursorStack.isEmpty()) {
-            RenderSystem.translatef(0.0f, 0.0f, 32.0f);
-            setZOffset(200); // ? questionable
-            itemRenderer.zOffset = 200.0f;
-            itemRenderer.renderInGuiWithOverrides(cursorStack, mouseX - 9, mouseY - 9);
-            itemRenderer.renderGuiItemOverlay(
-                    textRenderer,
-                    cursorStack,
-                    mouseX - 9,
-                    mouseY - 9,
-                    cursorStack.getCount() > 1 ? String.format("%d", cursorStack.getCount()) : null
-            );
-            setZOffset(0); // ? questionable
-            itemRenderer.zOffset = 0.0f;
-        }
-//        renderTooltip(matrices, mouseX, mouseY)
+    TextureSetting getTexture() {
+        return BG_TEXTURE;
     }
 }
