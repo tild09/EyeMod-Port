63777a2b9b611bcb341bd1941519ab7d4248d60a
Ye Yu <rafolwen98@gmail.com>
2020-11-10 20:39:02 +0800

    Swing hand on use dummy item

--- src/main/java/me/fabric/eyephonemod/item/DummyItem.java
+++ src/main/java/me/fabric/eyephonemod/item/DummyItem.java
@@ -1,9 +1,38 @@
 package me.fabric.eyephonemod.item;
 
-import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
-public class DummyItem extends Item {
+public final class DummyItem extends TaggedItem {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public DummyItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public CompoundTag deserializeStack(@NotNull CompoundTag compoundTag) {
+        LOGGER.info("Deserializing dummy item");
+        if (!compoundTag.contains("isDummy")) LOGGER.info("Item has not yet been tagged. Try saving and reloading the world.");
+        else LOGGER.info("Item has the tag 'isDummy': {}", compoundTag.getString("isDummy"));
+        return compoundTag;
+    }
+
+    @Override
+    public void serializeStack(@NotNull ItemStack itemStack, @NotNull CompoundTag compoundTag) {
+        LOGGER.info("Serializing dummy item");
+        compoundTag.putString("isDummy", "yes is it dummy");
+    }
+
+    @Override
+    public boolean interact(World world, PlayerEntity user, Hand hand, ItemStack itemStack) {
+        return true;
     }
 }
---
+++ src/main/java/me/fabric/eyephonemod/mixin/ItemStackMixin.java
@@ -0,0 +1,22 @@
+package me.fabric.eyephonemod.mixin;
+
+import me.fabric.eyephonemod.item.TaggedItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    @Shadow private CompoundTag tag;
+
+    @Inject(method = "toTag", at = @At("HEAD"))
+    private void onToTag(CompoundTag incomingTag, CallbackInfoReturnable<CompoundTag> cir) {
+        if (!(((ItemStack) (Object) this).getItem() instanceof TaggedItem)) return;
+        if (tag == null) return;
+        ((TaggedItem) ((ItemStack) (Object) this).getItem()).serializeStack((((ItemStack) (Object) this)), tag);
+    }
+}
