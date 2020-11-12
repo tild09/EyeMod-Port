dfc4f5ecdb19d5fbde1ba23d32f7db358bbadecb
Ye Yu <rafolwen98@gmail.com>
2020-11-12 08:49:25 +0800

    Add phone and case variants

--- src/main/java/me/fabric/eyephonemod/item/ItemRegistry.java
+++ src/main/java/me/fabric/eyephonemod/item/ItemRegistry.java
@@ -13,17 +13,55 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public enum ItemRegistry {
-    // phones variation
-    BATTERY(DummyItem::new),
+    // deprecated
     BLACK_COVER(DummyItem::new),
     COVER(DummyItem::new),
-    DISPLAY(DummyItem::new),
-    ENGINE(DummyItem::new),
+    RED_DISPLAY(DummyItem::new),
     EYEPHONE(EyePhoneItem::new),
     EYEPHONE_OP(DummyItem::new),
     OP_CHIP(DummyItem::new),
-    RED_DISPLAY(DummyItem::new),
-    WIRE(DummyItem::new);
+
+    // phones components
+    DISPLAY(DummyItem::new),
+    BATTERY(DummyItem::new),
+    ENGINE(DummyItem::new),
+    WIRE(DummyItem::new),
+
+    // cases
+    CASE_BLACK(DummyItem::new),
+    CASE_BLUE(DummyItem::new),
+    CASE_BROWN(DummyItem::new),
+    CASE_CYAN(DummyItem::new),
+    CASE_GRAY(DummyItem::new),
+    CASE_GREEN(DummyItem::new),
+    CASE_LIGHT_BLUE(DummyItem::new),
+    CASE_LIGHT_GRAY(DummyItem::new),
+    CASE_LIME(DummyItem::new),
+    CASE_MAGENTA(DummyItem::new),
+    CASE_ORANGE(DummyItem::new),
+    CASE_PINK(DummyItem::new),
+    CASE_PURPLE(DummyItem::new),
+    CASE_RED(DummyItem::new),
+    CASE_WHITE(DummyItem::new),
+    CASE_YELLOW(DummyItem::new),
+
+    // phones
+    PHONE_BLACK(EyePhoneItem::new),
+    PHONE_BLUE(EyePhoneItem::new),
+    PHONE_BROWN(EyePhoneItem::new),
+    PHONE_CYAN(EyePhoneItem::new),
+    PHONE_GRAY(EyePhoneItem::new),
+    PHONE_GREEN(EyePhoneItem::new),
+    PHONE_LIGHT_BLUE(EyePhoneItem::new),
+    PHONE_LIGHT_GRAY(EyePhoneItem::new),
+    PHONE_LIME(EyePhoneItem::new),
+    PHONE_MAGENTA(EyePhoneItem::new),
+    PHONE_ORANGE(EyePhoneItem::new),
+    PHONE_PINK(EyePhoneItem::new),
+    PHONE_PURPLE(EyePhoneItem::new),
+    PHONE_RED(EyePhoneItem::new),
+    PHONE_WHITE(EyePhoneItem::new),
+    PHONE_YELLOW(EyePhoneItem::new);
 
     public final String path;
     private volatile Item item = null;
